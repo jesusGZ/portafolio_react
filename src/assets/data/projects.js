f8e164baffe3f31272bc84efad380c5315bb6029
jesusGZ <j.gr.zurita@gmail.com>
2021-07-27 13:19:29 -0500

    Agregado de recursos

---
+++ src/assets/data/projects.js
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from 'uuid';
+import ProjectImg from '../images/projectImg.png';
+import UTrackerImg from '../images/utracker.jpg';
+import GreenCtgImg from '../images/greenctg.jpg';
+import CoinTrackerImg from '../images/cointracker.jpg';
+import CavinImg from '../images/cavinimg.jpg';
+
+const projects = [
+  {
+    id: uuidv4(),
+    name: 'xxxx',
+    desc: 'rthhjkolpklkuyjthyjhukjiokp',
+    img: UTrackerImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'gfvjhuk',
+    desc: 'gfvnbjkllkufthjkñpjjyui.',
+    img: GreenCtgImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'ffff',
+    desc: 'fgvjklkuyrtghjkñkjtyuhj',
+    img: CoinTrackerImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'hnkjmklk',
+    desc: 'fgyhujiokplkytrerftgyhujikolploiuyutyghujikolpiuyuim,',
+    img: CavinImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'Tefrf',
+    desc: 'Ahtghjojhtyhjkliuyuhjkyuij.',
+    img: ProjectImg,
+  },
+];
+
+export default projects;
---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,24 @@
+const testimonials = [
+  {
+    id: 1,
+    name: 'wfrfwf',
+    title: 'CEO',
+    org: 'WEB tgrt',
+    desc: 'jykhijihioiyhjoreglirejkokoger.',
+  },
+  {
+    id: 2,
+    name: 'frgtgt',
+    title: 'Co-founder',
+    org: 'tgrt',
+    desc: 'wrtghhryjyrhrghsehre.',
+  },
+  {
+    id: 3,
+    name: 'grtsgt',
+    title: 'RH',
+    org: 'trt',
+    desc: 'rthdytdyjutjujugjfhfgd.',
+  },
+];
+export default testimonials;
